2425e0e18d4923bfe0a96ef8d7edd7ffc6e9164e
AlexReyCer <ale.rey.cer@gmail.com>
2022-04-28 10:36:13 -0500

    Created a function to randomly choose between Rock,Paper Scissors

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+function computerPlay(max){
+    let decision = Math.floor(Math.random() * max);
+    if (decision == 0) {
+        console.log("Rock");
+    }
+    else if (decision == 1) {
+        console.log("Paper")
+    }
+    else {
+        console.log ("Scissors")
+    }
+}
+console.log(computerPlay(3));
