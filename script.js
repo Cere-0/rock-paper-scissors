a7432123b7c7df84537e1c4730596ffbec99c250
AlexReyCer <ale.rey.cer@gmail.com>
2022-04-29 10:53:16 -0500

    Added a function that plays 1 round against the computer and ask the user for input

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-function computerPlay (){ //Choses a number between 0 and 2 and depending on the number return rock,paper or scissors
+function computerPlay (){ //Choses a number between 0 and 2 and depending on the number return ROCK,PAPER or SCISSORS
     let number = Math.floor(Math.random() * 3);
     if (number == 0){ 
-        return "rock";
+        return "ROCK";
     }else if (number == 1){
-        return "paper";
+        return "PAPER";
     }else{
-        return "scissors"
+        return "SCISSORS"
     }
 }
-//console.log(computerPlay());
-const playerSelection =  prompt('Choose Rock, Paper or Scissors:').toLowerCase(); //Ask the user for imput and converts it to a lowercase string.
 
-console.log(playerSelection);
+
+function playRound(playerSelection, computerSelection) { //Plays a round against the computer
+    if (playerSelection === computerSelection) {
+        return 'Its a tie';
+    }
+    else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' || playerSelection === 'PAPER' && computerSelection === 'ROCK' || playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        return 'YOU WIN';
+    }
+    else {
+        return 'YOU LOSE';
+    }
+}
+
+const playerSelection =  prompt('Choose ROCK, PAPER or SCISSORS:').toUpperCase(); //Ask the user for imput and converts it to a lowercase string.
+
+console.log('You choose ' + playerSelection); //User choice
 
 const computerSelection = computerPlay();
 
-function playRound(playerSelection, computerSelection) {
+console.log('Computer chooses ' + computerSelection);//Computer choice
 
-}
+console.log(playRound(playerSelection,computerSelection));
