0244536e43c9af68ba65641dd5e6f1cee425caf3
AlexReyCer <ale.rey.cer@gmail.com>
2022-04-29 15:16:12 -0500

    Aded a code to show rounds losed, winned and tied, and show a message of who the winner is

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function computerPlay (){ //Choses a number between 0 and 2 and depending on the
     }
 }
 
-
 function playRound(playerSelection, computerSelection) { //Plays a round against the computer
     if (playerSelection === computerSelection) {
         return 'IT\'S A TIE';
@@ -21,25 +20,37 @@ function playRound(playerSelection, computerSelection) { //Plays a round against
         return 'YOU LOSE';
     }
 }
-function game() {
+function game() { //Function that play 5 rounds of rock paper scissors
     for (let i = 0; i < 5; i++) {
         const playerSelection =  prompt('Choose ROCK, PAPER or SCISSORS:').toUpperCase();
         const computerSelection = computerPlay();
         console.log('You choose ' + playerSelection); //User choice
         console.log('Computer chooses ' + computerSelection);//Computer choice
+        if (playRound(playerSelection,computerSelection) === 'YOU WIN'){
+            roundWin +=1;
+        }
+        else if (playRound(playerSelection,computerSelection) === 'YOU LOSE'){
+            roundLose +=1;
+        }
+        else if (playRound(playerSelection,computerSelection) === 'IT\'S A TIE') {
+            roundTie +=1;
+        }
         console.log(playRound(playerSelection,computerSelection))
         
     }
 }
+let roundWin = 0;
+let roundLose = 0;
+let roundTie = 0;
 
-//const playerSelection =  prompt('Choose ROCK, PAPER or SCISSORS:').toUpperCase(); //Ask the user for imput and converts it to a lowercase string.
-
-//console.log('You choose ' + playerSelection); //User choice
-
-//const computerSelection = computerPlay();
-
-//console.log('Computer chooses ' + computerSelection);//Computer choice
-
-//console.log(playRound(playerSelection,computerSelection));
-
-console.log(game());
+console.log(game(''));
+console.log('Rounds winned: ' + roundWin, 'Rounds losed: ' +  roundLose, 'Rounds tied: ' +  roundTie);
+if (roundWin > roundLose){
+    console.log('WINNER');
+}
+else if (roundLose > roundWin) {
+    console.log('COMPUTER WINS');
+}
+else {
+    console.log('TIE');
+}
